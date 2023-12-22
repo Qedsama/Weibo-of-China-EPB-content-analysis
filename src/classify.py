c961808db8c711004ce0447223ba941f5ad72121
Qedsama <2280027610@qq.com>
2023-12-22 16:30:29 +0800

    fix: about classify

--- src/classify.py
+++ src/classify.py
@@ -5,9 +5,8 @@ import jieba
 import string
 import re
 import gensim
-import concurrent.futures
-import csv
 
+YEAR ="2019"
 def generate_vocab_and_vectors(model):
     vocab = {}
     default_vector = np.zeros_like(model['我']) 
@@ -33,40 +32,26 @@ def preprocess_text(text, vocab, default_vector):
     input_array = np.array(input_vector)
     return np.expand_dims(input_array, axis=0)
 
-def classify_text(model, text, vocab, default_vector):
-    preprocessed_input = preprocess_text(text, vocab, default_vector)
+def classify_text(model, texts, vocab, default_vector):
+    preprocessed_inputs = [] 
+    for text in texts:
+        preprocessed_inputs.append(preprocess_text(text[1]['微博正文'], vocab, default_vector))
+    preprocessed_input = np.vstack(preprocessed_inputs)  
     prediction = model.predict(preprocessed_input)
-    return 1 if prediction > 0.5 else 0
+    prediction = [1 if value > 0.5 else 0 for value in prediction]
+    return prediction
 
-def process_weibo_text(args):
-    weibo_text, model, vocab, default_vector = args
-    return classify_text(model, weibo_text['微博正文'], vocab, default_vector)
 
 if __name__ == "__main__":
 
-    word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(r'../bin/wiki_word2vec_50.bin', binary=True)
+    word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(r'wiki_word2vec_50.bin', binary=True)
     vocab, default_vector = generate_vocab_and_vectors(word2vec_model)
-
-
     model = tf.keras.models.load_model('model.h5')
-
-
-    df = pd.read_csv('../data/merged_data_2015.csv', encoding='utf-8')
-    #更换为对应年份的数据
-
-
+    df = pd.read_csv('./data/merged_data_'+YEAR+'.csv', encoding='utf-8')
     transferred_data = df[df['微博正文'].str.contains('转发内容')]
-    transferred_data.to_csv('./results/transferred_data_2015.csv', mode='a', header=False, index=False, encoding='utf-8')
-    #更换为对应年份的数据
-
+    transferred_data.to_csv('./results/transferred_data_'+YEAR+'.csv', mode='a', header=False, index=False, encoding='utf-8')
     df = df[~df['微博正文'].str.contains('转发内容')]
-
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        num_threads = 4
-        results = list(executor.map(process_weibo_text, [(row, model, vocab, default_vector) for _, row in df.iterrows()]))
-
+    results=classify_text(model, df.iterrows(), vocab, default_vector)
     df['分类结果'] = results
     df = df[['分类结果'] + [col for col in df.columns if col != '分类结果']]
-    df.to_csv('./results/result_2015.csv', index=False, encoding='utf-8')
-    #更换为对应年份的数据
+    df.to_csv('./results/result_'+YEAR+'.csv', index=False, encoding='utf-8')
